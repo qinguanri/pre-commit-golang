b51b6e2c305de79236234b3911d1c51977fc1231
覃冠日 <403709339@qq.com>
2018-02-06 20:52:53 +0800

    Update run-go-lint.sh

--- run-go-lint.sh
+++ run-go-lint.sh
@@ -3,13 +3,9 @@
 # Capture and print stdout/stderr, since golint doesn't use proper exit codes
 #
 set -e
-golint_cmd=`which golint`
-if [ "$golint_cmd" == "" ]; then
-    golint_cmd="golint"
-fi
 
 exec 5>&1
 for file in "$@"; do
-    output="$(golint_cmd "$file" 2>&1 | tee /dev/fd/5)"
+    output="$(golint "$file" 2>&1 | tee /dev/fd/5)"
     [[ -z "$output" ]]
 done
