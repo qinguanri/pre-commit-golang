66485e3ed117e9ab29ef8cc41c31369cf7d835aa
覃冠日 <403709339@qq.com>
2018-02-06 21:14:32 +0800

    Update run-go-lint.sh

--- run-go-lint.sh
+++ run-go-lint.sh
@@ -6,6 +6,6 @@ set -e
 
 exec 5>&1
 for file in "$@"; do
-    output="$(golint "$file" 2>&1 | tee /dev/fd/5)"
+    output="$(/usr/local/bin/golint "$file" 2>&1 | tee /dev/fd/5)"
     [[ -z "$output" ]]
 done
